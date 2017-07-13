035031a911472a7d75b78058c12b4ac4dbf62e4c
Ian Harris <ian@fofgof.xyz>
2017-07-12 20:17:39 -0700

    Reading and printing functions outlined
    It's fucking ugly. I'm just copying everything. To do it correctly is escaping
    me currently. Surely will be refactored later.

--- cafebabe-fp.c
+++ cafebabe-fp.c
@@ -54,20 +54,108 @@ u4 read_u4(FILE *f) {
     return tmp;
 }
 
+struct cp_printing {
+    int tag;
+    void (*func)(cp_info *);
+};
+
+void print_constant_class_info(cp_info *cp) {
+    printf("Class| not yet implemented.\n");
+}
+
+void print_constant_fieldref_info(cp_info * cp) {
+    printf("Fieldref| not yet implemented.\n");
+}
+
+void print_constant_methodref_info(cp_info * cp) {
+    printf("Methodref| tag: %u; class_index: %u; name_and_type_index: %u\n",
+            cp->tag,
+            ((CONSTANT_Methodref_info *)cp)->class_index,
+            ((CONSTANT_Methodref_info *)cp)->name_and_type_index);
+
+}
+
+void print_constant_interfacemethodref_info(cp_info * cp) {
+    printf("InterfaceMethodref| not yet implemented.\n");
+}
+
+void print_constant_string_info(cp_info * cp) {
+    printf("String| not yet implemented.\n");
+}
+
+void print_constant_integer_info(cp_info * cp) {
+    printf("Integer| not yet implemented.\n");
+}
+
+void print_constant_float_info(cp_info * cp) {
+    printf("Float| not yet implemented.\n");
+}
+
+void print_constant_long_info(cp_info * cp) {
+    printf("Long| not yet implemented.\n");
+}
+
+void print_constant_double_info(cp_info * cp) {
+    printf("Double| not yet implemented.\n");
+}
+
+void print_constant_nameandtype_info(cp_info * cp) {
+    printf("NameAndType| not yet implemented.\n");
+}
+
+void print_constant_utf8_info(cp_info * cp) {
+    printf("Utf8| not yet implemented.\n");
+}
+
+void print_constant_methodhandle_info(cp_info * cp) {
+    printf("MethodHandle| not yet implemented.\n");
+}
+
+void print_constant_methodtype_info(cp_info * cp) {
+    printf("MethodType| not yet implemented.\n");
+}
+
+void print_constant_invokedynamic_info(cp_info * cp) {
+    printf("InvokeDynamic| not yet implemented.\n");
+}
+
+struct cp_printing cp_print[] = {
+    { 0, 0},
+    { CONSTANT_Utf8,               print_constant_utf8_info               },
+    { 0, 0},
+    { CONSTANT_Integer,            print_constant_integer_info            },
+    { CONSTANT_Float,              print_constant_float_info              },
+    { CONSTANT_Long,               print_constant_long_info               },
+    { CONSTANT_Double,             print_constant_double_info             },
+    { CONSTANT_Class,              print_constant_class_info              },
+    { CONSTANT_String,             print_constant_string_info             },
+    { CONSTANT_Fieldref,           print_constant_fieldref_info           },
+    { CONSTANT_Methodref,          print_constant_methodref_info          },
+    { CONSTANT_InterfaceMethodref, print_constant_interfacemethodref_info },
+    { CONSTANT_NameAndType,        print_constant_nameandtype_info        },
+    { 0, 0},
+    { 0, 0},
+    { CONSTANT_MethodHandle,       print_constant_methodhandle_info       },
+    { CONSTANT_MethodType,         print_constant_methodtype_info         },
+    { 0, 0},
+    { CONSTANT_InvokeDynamic,      print_constant_invokedynamic_info      }
+};
+
 // XXX: This is not scalable or maintanable.
 void print_constant_pool(cp_info *cp) {
     if (!cp) {
         printf("haven't read this cp yet\n");
         return;
     }
-    switch (cp->tag) {
-        case CONSTANT_Methodref: printf("method ref| tag: %u; class_index: %u; name_and_type_index: %u\n",
-                                         cp->tag,
-                                         ((CONSTANT_Methodref_info *)cp)->class_index,
-                                         ((CONSTANT_Methodref_info *)cp)->name_and_type_index);
-                                 break;
-        default: printf("not yet implemented\n");
-    }
+    cp_print[cp->tag].func(cp);
+//    switch (cp->tag) {
+//        case CONSTANT_Methodref: printf("method ref| tag: %u; class_index: %u; name_and_type_index: %u\n",
+//                                         cp->tag,
+//                                         ((CONSTANT_Methodref_info *)cp)->class_index,
+//                                         ((CONSTANT_Methodref_info *)cp)->name_and_type_index);
+//                                 break;
+//        default: printf("not yet implemented\n");
+//    }
 }
 
 void print_interfaces(u2 *i) {
@@ -88,10 +176,10 @@ void print_attributes(attribute_info *ai) {
 
 void print_cf(struct class_file *cf) {
     printf(
-"magic: %X\n\
-minor_version: %u\n\
-major_version: %u\n\
-constant_pool_count: %u\n",
+            "magic: %X\n\
+            minor_version: %u\n\
+            major_version: %u\n\
+            constant_pool_count: %u\n",
             cf->magic,
             cf->minor_version,
             cf->major_version,
@@ -105,10 +193,10 @@ constant_pool_count: %u\n",
     }
 
     printf(
-"access_flags: %u\n\
-this_class: %u\n\
-super_class: %u\n\
-interfaces_count: %u\n",
+            "access_flags: %u\n\
+            this_class: %u\n\
+            super_class: %u\n\
+            interfaces_count: %u\n",
             cf->access_flags,
             cf->this_class,
             cf->super_class,
@@ -174,36 +262,14 @@ void read_constant_pool_count(struct class_file *cf) {
     cf->next = read_constant_pool;
 }
 
-void read_constant_class_info(struct class_file *cf, int index) {
-
+void read_class_info(struct class_file *cf, int index) {
+    cf->constant_pool[index] = malloc(sizeof(CONSTANT_Class_info));
+    CONSTANT_Class_info *tmp = (CONSTANT_Class_info *)cf->constant_pool[index];
+    tmp->tag = CONSTANT_Class;
+    tmp->name_index = read_u2(cf->file);
 }
 
-void read_constant_fieldref_info(struct class_file *cf, int index) {
-
-}
-
-void read_constant_methodref_info(struct class_file *cf, int index) {
-
-}
-
-typedef struct CONSTANT_NameAndType_info {
-    u1 tag;
-    u2 name_index;
-    u2 descriptor_index;
-} CONSTANT_NameAndType_info;
-
-// I'll have to look at the struct stacking polymorphism deal for this.
-
-void read_nameandtype_info(struct class_file *cf, int index) {
-    // set tag
-    // will have value 12 always (0x0a)
-    //cf->constant_pool[index] = malloc(sizeof(CONSTANT_NameAndType_info));
-    //cf->constant_pool[index].tag = 12;
-    //cf->constant_pool[index].name_index = read_u2(cf->file);
-    //cf->constant_pool[index].descriptor_index = read_u2(cf->file);
-}
-
-void unknown(struct class_file *cf, int index) {
+void read_fieldref_info(struct class_file *cf, int index) {
 
 }
 
@@ -215,12 +281,74 @@ void read_methodref_info(struct class_file *cf, int index) {
     self->name_and_type_index = read_u2(cf->file);
 }
 
+void read_interfacemethodref_info(struct class_file *cf, int index) {
+
+}
+
+void read_string_info(struct class_file *cf, int index) {
+
+}
+
+void read_integer_info(struct class_file *cf, int index) {
+
+}
+
+void read_float_info(struct class_file *cf, int index) {
+
+}
+
+void read_long_info(struct class_file *cf, int index) {
+
+}
+
+void read_double_info(struct class_file *cf, int index) {
+
+}
+
+void read_nameandtype_info(struct class_file *cf, int index) {
+    cf->constant_pool[index] = malloc(sizeof(CONSTANT_NameAndType_info));
+    CONSTANT_NameAndType_info *tmp = (CONSTANT_NameAndType_info *)cf->constant_pool[index];
+    tmp->tag = CONSTANT_NameAndType;
+    tmp->name_index = read_u2(cf->file);
+    tmp->descriptor_index = read_u2(cf->file);
+}
+
+void read_utf8_info(struct class_file *cf, int index) {
+
+}
+
+void read_methodhandle_info(struct class_file *cf, int index) {
+
+}
+
+void read_methodtype_info(struct class_file *cf, int index) {
+
+}
+
+void read_invokedynamic_info(struct class_file *cf, int index) {
+
+}
+
+void unknown(struct class_file *cf, int index) {
+
+}
+
 void (*read_tag(int c))(struct class_file *, int) {
     switch (c) {
-        case CONSTANT_Class: return read_constant_class_info;
-        case CONSTANT_Fieldref: return read_constant_fieldref_info;
-        case CONSTANT_NameAndType: return read_nameandtype_info;
+        case CONSTANT_Class: return read_class_info;
+        case CONSTANT_Fieldref: return read_fieldref_info;
         case CONSTANT_Methodref: return read_methodref_info;
+        case CONSTANT_InterfaceMethodref: return read_interfacemethodref_info;
+        case CONSTANT_String: return read_string_info;
+        case CONSTANT_Integer: return read_integer_info;
+        case CONSTANT_Float: return read_float_info;
+        case CONSTANT_Long: return read_long_info;
+        case CONSTANT_Double: return read_double_info;
+        case CONSTANT_NameAndType: return read_nameandtype_info;
+        case CONSTANT_Utf8: return read_utf8_info;
+        case CONSTANT_MethodHandle: return read_methodhandle_info;
+        case CONSTANT_MethodType: return read_methodtype_info;
+        case CONSTANT_InvokeDynamic: return read_invokedynamic_info;
         default: return unknown;
     }
 }
